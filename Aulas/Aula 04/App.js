93bc14423d969f63249656ad53a5a160ce53df1d
lucascabralds <lucascabraldossantos53@gmail.com>
2021-09-23 00:16:16 -0300

    Em nossa quarta aula aprendemos alguns CSS que podemos implementa dentro do nosso CSS

---
+++ Aulas/Aula 04/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native'; 
+
+class App extends Component{
+  render(){
+    return(
+      <View style={{flex:1, backgroundColor: '#222'}}>
+
+          <View style={{height: 65, backgroundColor: '#222'}}></View>
+
+          <View style={{flex:1, backgroundColor: '#FFF'}}></View>
+
+          <View style={{height:65, backgroundColor: '#222'}}></View>
+      </View>
+    );
+  }
+}
+
+
+export default App;
