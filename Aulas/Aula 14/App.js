74fbeaa9179788f4afc5c965873fc5e1034beb1d
lucascabralds <lucascabraldossantos53@gmail.com>
2021-09-23 00:24:07 -0300

    Em nossa décima quarta aula aprendemos tudo que está voltado ao SWITCH, valores, e também como podemos customizar e declarar em ferramenta, aprendemos também um novo comanmdo de decisão em texto que seria a inserção de somente 2 ponto em um TEXT

---
+++ Aulas/Aula 14/App.js
@@ -0,0 +1,41 @@
+import React, {Component } from 'react';
+import {View,
+        Text,
+        StyleSheet,
+        Switch
+} from 'react-native';
+
+class App extends Component{
+  constructor(props){
+  super(props);
+    this.state={
+      status:false
+     };
+      
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+
+        <Switch
+        value={this.state.status}
+        onValueChange={(valorSwitch) => this.setState({status:valorSwitch})}
+        thumbColor="#FF0000"
+        />
+        <Text>
+          {(this.state.status) ? "Ativo" : "Inativo"}</Text>
+
+      </View>
+    );
+  }
+}
+
+const styles =StyleSheet.create({
+  container:{
+    flex:1,
+    marginTop:15
+  },
+})
+
+export default App;
