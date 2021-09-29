2ec7ab254de32dab6369656360685d4857743778
lucascabralds <lucascabraldossantos53@gmail.com>
2021-09-29 19:25:02 -0300

    Em nossa quinta aula dentro de nossa quinta aula aprendemos algumas timeline de codigo como FLEXDIRECTION,ALIGNITEMS é JUSTIFYCONTENT que vão ser bastante importante a longo prazo da customização do nosso aplicativo, aprendemos também o conceito de largura e altura dentro de nossas view.

--- Aulas/Aula 05/App.js
+++ Aulas/Aula 05/App.js
@@ -9,6 +9,13 @@ class App extends Componet{
                 alignItems: "flex-end",
                 justifyContent: "center"}}>
 
+                <View style = {{ width: 50, height: 50, backgroundColor: "green"}}></View>
+
+                <View style = {{ width: 50, height: 50, backgroundColor: "red"}}></View>
+
+                <View style = {{ width: 50, height: 50, backgroundColor: "yellow"}}></View>
+
+
                 </View>
         )
     }
