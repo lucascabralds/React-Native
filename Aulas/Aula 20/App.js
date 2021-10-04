373773ac4c1fd29d7877d930534404edc6495c24
lucascabralds <lucascabraldossantos53@gmail.com>
2021-10-04 15:56:51 -0300

    Em nossa vigésima aula aprendemos o conceito de REQUISIÇÕES HTTP instalando em nosso projeto a extensão AXIOS.

---
+++ Aulas/Aula 20/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {View,
+        StyleSheet,
+      FlatList} from 'react-native';
+
+import api from './src/services/api';
+import Filmes from './src/Filmes';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      filmes:[]
+    };
+  }
+
+async componentDidMount(){
+  const response = await api.get('r-api/?api=filmes/');
+  this.setState({
+    filmes:response.data
+  });
+}
+  render(){
+    return(
+      <View style={styles.container}>
+
+
+          <FlatList data={this.state.filmes}
+                    keyExtractor={item=> item.id.toString()}
+                    renderItem={({item}) => <Filmes data={item}/>}/>
+      </View>
+    );
+  }
+}
+
+const styles= StyleSheet.create({
+  container:{
+    flex:1,
+  }
+});
+
+export default App;
