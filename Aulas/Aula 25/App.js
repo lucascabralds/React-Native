a79fe1defa0ebe58f85667306b999c0229ee8b2b
lucascabralds <lucascabraldossantos53@gmail.com>
2021-10-25 12:05:25 -0300

    Em nossa vigésima quinta aula estamos aprendendo mais o conceito de ANIMAÇÂO dentro de nossa ferramenta React-Native, e dentro dessa aula aprendemos a realizar sequencia de animação em conjunto ou também por ações.

---
+++ Aulas/Aula 25/App.js
@@ -0,0 +1,88 @@
+import React,{Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated } from 'react-native';
+
+export default class App extends Component{
+
+constructor(props){
+  super(props);
+  this.state={
+      LarAnimada:new Animated.Value(150),
+      AltAnimada:new Animated.Value(50),
+      OpacidadeAnimada: new Animated.Value(0)
+
+  };
+
+  Animated.sequence([
+    
+    Animated.timing(
+      this.state.OpacidadeAnimada,
+      {
+        toValue:1,
+        duration:1000
+      }
+    ),
+
+    Animated.parallel([
+
+        Animated.timing(
+        this.state.LarAnimada,
+        {
+          toValue:300,
+          duration:1500
+        }
+      ),
+
+      Animated.timing(
+        this.state.AltAnimada,
+        {
+          toValue:200,
+          duration:1500
+        }
+      )
+      
+    ]),
+
+        Animated.timing(
+      this.state.OpacidadeAnimada,
+      {
+        toValue:0,
+        duration:1000
+      }
+    )
+  ]).start();
+
+  }
+  render(){
+
+  return (
+    <View style={styles.container}>
+
+        <Animated.View style={{width:this.state.LarAnimada,
+                      height:this.state.AltAnimada,
+                      backgroundColor:'#4169E1',
+                      justifyContent:'center' ,
+                      opacity:this.state.OpacidadeAnimada}}>
+
+          <Text style={{color:'#FFFF',fontSize:22,textAlign:'center'}}>
+            Carregando...
+          </Text>
+
+        </Animated.View>
+
+    </View>
+  );
+}
+}
+
+
+const styles = StyleSheet.create({
+ container:{
+   flex:1,
+   justifyContent:'center',
+   alignItems:'center',
+ },
+});
