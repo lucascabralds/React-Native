7e0d6e6e648d00dca6b046663287b0fd6709444c
lucascabralds <lucascabraldossantos53@gmail.com>
2021-10-25 18:06:07 -0300

    Em nossa vigésima setima aula estamos aprendendo mais o conceito de ANIMAÇÂO dentro de nossa ferramenta React-Native, e dentro dessa aula aplicamos nossos conhecimento de animação em graficos, e aplicando esse animação atraves de um botão.

---
+++ Aulas/Aula 27/App.js
@@ -0,0 +1,99 @@
+import { opaqueType, TERMINATORLESS_TYPES } from '@babel/types';
+import React,{Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableOpacity, 
+  TouchableOpacityBase} from 'react-native';
+
+export default class App extends Component{
+
+constructor(props){
+  super(props);
+  this.state={
+      LarAnimada:new Animated.Value(150),
+      AltAnimada:new Animated.Value(50),
+      OpAnimada: new Animated.Value(0)
+
+  };
+
+  this.carregarGrafico=this.carregarGrafico.bind(this);
+  
+  }
+
+  carregarGrafico(){
+
+      Animated.sequence([
+
+        Animated.timing(
+            this.state.OpAnimada,
+            {
+              toValue:1,
+              duration:400
+            }
+        ),
+          Animated.timing(
+            this.state.AltAnimada,
+            {
+              toValue: 300,
+              duration: 1000
+            }
+          )
+      ]).start();
+
+  }
+  render(){
+
+  return (
+    <View style={styles.container}>
+
+      <View style={{height:80,
+                    alignItems:'center',
+                    justifyContent:'center',
+                    flexDirection:'row',
+                    backgroundColor:'#4169E1'}}>
+
+          <TouchableOpacity onPress={this.carregarGrafico}>
+            <Text  style={{fontSize:25,
+                           color:'#FFF'}}>
+              Gerar Grafico
+            </Text>
+          </TouchableOpacity>
+
+      </View>
+
+      <View style={{flex:1,
+                    flexDirection:'column',
+                    justifyContent:'flex-end',
+                    alignItems:'center'}}>
+
+          <Text> Vendas </Text>
+
+        <Animated.View style={ {width:this.state.LarAnimada,
+                               height:this.state.AltAnimada,
+                               backgroundColor:'#FF0000',
+                               justifyContent:'center',
+                               opacity:this.state.OpAnimada}}>
+
+          <Text style={{  color:'#FFFF',
+                          fontSize:20,
+                          textAlign:'center'}}> R$150,00</Text>
+
+        </Animated.View>
+      </View>
+
+        
+
+    </View>
+  );
+}
+}
+
+
+const styles = StyleSheet.create({
+ container:{
+   flex:1,
+ },
+});
