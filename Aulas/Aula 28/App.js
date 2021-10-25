e9662e98b7f83cbce41d4e2f4e17b1c4e5c5a9aa
lucascabralds <lucascabraldossantos53@gmail.com>
2021-10-25 20:49:32 -0300

    Em nossa vigésima setima aula estamos aprendendo mais o conceito de ANIMAÇÂO dentro de nossa ferramenta React-Native, e dentro dessa aula aplicamos nossos conhecimento de animação em graficos, e agora aprendemos mais o conceito de Interpolações dentro do nossoa programa, que seria o uso de Porcentagem(%)

---
+++ Aulas/Aula 28/App.js
@@ -0,0 +1,62 @@
+import { opaqueType, TERMINATORLESS_TYPES } from '@babel/types';
+import React,{Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated} from 'react-native';
+
+export default class App extends Component{
+
+constructor(props){
+  super(props);
+  this.state={
+      LarAnimada:new Animated.Value(0),
+      AltAnimada:new Animated.Value(50),
+      OpAnimada: new Animated.Value(0)
+
+  };
+  
+    Animated.timing(
+      this.state.LarAnimada,
+      {
+        toValue:100,
+        duration:4000
+      }
+    ).start();
+  
+  
+
+  }
+  render(){
+
+    let porcentagemAnimate = this.state.LarAnimada.interpolate({
+        inputRange:[0,100],
+        outputRange:['0%','100%']
+        
+    });
+
+  return (
+    <View style={styles.container}>
+
+      <Animated.View style={{backgroundColor:'#4169E1',
+                    width:porcentagemAnimate,
+                    height:25}}>
+
+
+      </Animated.View>
+
+
+    </View>
+  );
+}
+}
+
+
+const styles = StyleSheet.create({
+ container:{
+   flex:1,
+   justifyContent:'center',
+   alignItems:'flex-start'
+ },
+});
