0a95e0d1a928e75ae1090661d14441d655f27d6d
lucascabralds <lucascabraldossantos53@gmail.com>
2021-11-24 20:32:50 -0300

    Em nossa trigésima quinta aula aprendemos o conceito de navegação entre aplicativos.

---
+++ Aulas/Aula 35/src/pages/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Home() {
+  const navigation = useNavigation();
+
+  function irSobre(){
+    navigation.navigate('Sobre', { nome: 'Matheus', email:'matheus@gmail.com' } );
+  }
+
+ return (
+   <View>
+      <Text>Home</Text>
+      <Text>Bem vindo a tela home!!</Text>
+     <Button  title="Ir para Sobre"
+              onPress={irSobre}/>
+   </View>
+  );
+}
